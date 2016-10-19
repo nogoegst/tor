88b75f53198a2c47dca822a81afabbd5672d945f
Ivan Markin <twim@riseup.net>
2016-10-19 00:37:57 +0000

    Fixup unreachable code in rend_service_derive_key_digests()

--- src/or/rendservice.c
+++ src/or/rendservice.c
@@ -1233,12 +1233,12 @@ rend_services_add_filenames_to_lists(smartlist_t *open_lst,
 static int
 rend_service_derive_key_digests(struct rend_service_t *s)
 {
-  if (rend_get_service_id(s->private_key, s->service_id)<0) {
-    log_warn(LD_BUG, "Internal error: couldn't encode service ID.");
-    return -1;
-  }
   if (crypto_pk_get_digest(s->private_key, s->pk_digest)<0) {
     log_warn(LD_BUG, "Couldn't compute hash of public key.");
+    return -1;
+  }
+  if (rend_get_service_id(s->private_key, s->service_id)<0) {
+    log_warn(LD_BUG, "Internal error: couldn't encode service ID.");
     return -1;
   }
 
